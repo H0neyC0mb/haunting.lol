b592a89b2036d467d81a182886b99a5db1d1426c
Joyify <72810515+H0neyC0mb@users.noreply.github.com>
2024-10-10 19:38:48 -0700

    Add files via upload

--- js/type.js
+++ js/type.js
@@ -1,5 +1,5 @@
 new Typed('#typed',{
-    strings : [,'@w.by','https://puppje.lol/','who ru lawl','stop tryna be me'],
+    strings : [,'@w.by','https://puppje.lol/','who ru lawl','stop tryna be me','@Sob my fav skid'],
     typeSpeed : 30,
     delaySpeed : 90,
     showCursor: false,
